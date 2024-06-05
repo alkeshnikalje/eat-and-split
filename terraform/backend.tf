9f228849744e77d34af48b3a478f4a568277ca18
Alkesh Nikalje <alkeshnikalje@gmail.com>
2024-06-05 21:41:38 +0530

    update region in backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "my-app/terraform.tfstate"
-    region         = "us-east-1"  # specify your AWS region
+    region         = "us-east-2"  # specify your AWS region
     encrypt        = true
   }
 }
