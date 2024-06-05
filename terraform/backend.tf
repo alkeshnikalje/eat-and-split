91f9061dfb900881370ac0a888a721e690510772
Alkesh Nikalje <alkeshnikalje@gmail.com>
2024-06-05 21:51:13 +0530

    update backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "eat-and-split-terraform-state-bucket"
     key            = "my-app/terraform.tfstate"
-    region         = "us-east-2"  # specify your AWS region
+    region         = "us-east-1"  # specify your AWS region
     encrypt        = true
   }
 }
